95f96157d2b086526513d8b64e7cb22b312aa185
Sarah O'Dell <sarah.mo26@gmail.com>
2021-05-19 21:29:07 -0600

    route on app.py tested, connects; changed environment to development; added dependencies

--- app.py
+++ app.py
@@ -1,13 +1,65 @@
 from flask import Flask, jsonify
 
+# from resources.tickets import tickets
+# from resources.users import users
+
+import models
+
+from flask_cors import CORS 
+
+from flask_login import LoginManager
+
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+
 DEBUG = True
-PORT = 8000
+PORT = os.environ.get("PORT_FLASK")
+
+# app.secret_key = os.environ.get("FLASK_SECRET_KEY")
+
+# login_manager = LoginManager()
+
+# login_manager.init_app(app)
+
+# @login_manager.user_loader
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 	return 'hi'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
